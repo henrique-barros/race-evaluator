4be2db9376075c09cc46c24beb44c6fe150bc11a
henriquesb6 <henriquesb6@hotmail.com>
2019-09-06 22:43:24 -0300

    checking which racer arrived first and some refactoring

--- services/race_service.js
+++ services/race_service.js
@@ -7,16 +7,24 @@ export const raceService = () => {
   const raceRepositoryInstance = raceRepository();
   const fileServiceInstance = fileService();
 
+  const parseTimeCompleted = (timeCompletedString) => {
+    return moment(timeCompletedString, 'HH:mm:ss.SSS');
+  }
+
+  const parseLapEllapsedTime = (lapEllapsedTimeString) => {
+    return lapEllapsedTimeString.length === 10 ? 
+      moment.duration(`00:${lapEllapsedTimeString}`) :
+      moment.duration(`00:0${lapEllapsedTimeString}`);
+  }
+
   const saveLapBasedOnString = (lapString) => {
 
     let lapStringArray = lapString.split(/\s+/);
-    let timeCompleted = moment(lapStringArray[0], 'HH:mm:ss.SSS');
+    let timeCompleted = parseTimeCompleted(lapStringArray[0]);
     let racerId = lapStringArray[1];
     let racerName = lapStringArray[3];
     let lapNumber = lapStringArray[4];
-    let lapEllapsedTime = lapStringArray[5].length === 10 ? 
-      moment.duration(`00:${lapStringArray[5]}`) :
-      moment.duration(`00:0${lapStringArray[5]}`);
+    let lapEllapsedTime = parseLapEllapsedTime(lapStringArray[5]);
     let avgSpeed = lapStringArray[6];
 
     let lap = raceRepositoryInstance.saveLap(timeCompleted, racerId, racerName, lapNumber, lapEllapsedTime, avgSpeed);
@@ -32,22 +40,48 @@ export const raceService = () => {
     lapStrings.slice(1).map((lapString) => saveLapBasedOnString(lapString));
   }
 
+  const getStartOfTheRace = (timeCompleted, lapEllapsedTime) => {
+    return (timeCompleted).subtract(lapEllapsedTime);
+  }
+  const getEndOfTheRace = (timeCompleted, startOfTheRace) => {
+    let endOfTheRace = timeCompleted;
+    if (endOfTheRace.isBefore(startOfTheRace)) endOfTheRace.add(1, 'day');
+    return endOfTheRace;
+  }
+  
+  const getTotalTimeOfTheRace = (startOfTheRace, endOfTheRace) => {
+    return moment.utc(endOfTheRace.diff(startOfTheRace));
+  }
+  
+  const getRacerResultsSorted = (racerResults) => {
+    const sortedRaceResults = racerResults.sort((a, b) => a.totalTimeOfRace.diff(b.totalTimeOfRace));
+    return sortedRaceResults.map((result, i) => {
+      result.arrivalPosition = i + 1;
+      return result;
+    })
+  }
+
   const getRaceResultsbyPilot = () => {
     const racersData = raceRepositoryInstance.getLapsByPilot();
-    let racerIds = Object.keys(racersData);
-    return racerIds.reduce((acc, racerId) => {
+    const racerIds = Object.keys(racersData);
+    const racerResults = racerIds.reduce((acc, racerId) => {
       let racer = racersData[racerId];
-      let endOfTheRace = racer.laps[racer.laps.length -1].timeCompleted;
-      let startOfTheRace = (racer.laps[0].timeCompleted).subtract(racer.laps[0].lapEllapsedTime);
-      if (endOfTheRace.isBefore(startOfTheRace)) endOfTheRace.add(1, 'day');
+      let startOfTheRace = getStartOfTheRace(racer.laps[0].timeCompleted, racer.laps[0].lapEllapsedTime);
+      let endOfTheRace = getEndOfTheRace(racer.laps[racer.laps.length -1].timeCompleted, startOfTheRace);
+      let totalTimeOfRace = getTotalTimeOfTheRace(startOfTheRace, endOfTheRace);
       let data = {
         racerId: racerId,
         racerName: racer.name,
         numberOfLapsCompleted: racer.laps.length,
-        totalTimeOfRace: moment.utc(endOfTheRace.diff(startOfTheRace)).format('HH:mm:ss.SSS')
+        totalTimeOfRaceFormatted: totalTimeOfRace.format('HH:mm:ss.SSS'),
+        totalTimeOfRace: totalTimeOfRace
       }
       acc.push(data);
+      return acc;
     }, []);
+
+    const sortedRaceResults = getRacerResultsSorted(racerResults);
+    return sortedRaceResults;
   };
 
   return {
